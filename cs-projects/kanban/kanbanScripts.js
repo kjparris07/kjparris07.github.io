22537acace72d54254d11004b65a961613beaaa7
kyra <kjparris07@gmail.com>
2024-09-20 23:25:26 -0400

    added styling and fixed bugs

--- cs-projects/kanban/kanbanScripts.js
+++ cs-projects/kanban/kanbanScripts.js
@@ -1,7 +1,6 @@
 let draggedElement = null;
 let offsetX = 0;
 let offsetY = 0;
-let taskBase = "<button onclick='deleteTask(this)'>x</button>n";
 
 function move(event) {
     if (draggedElement) {
@@ -15,8 +14,13 @@ function move(event) {
 
 function mouseDown(event) {
     let div = event.target;
-    if (div.classList.contains("tasks")) {
-        draggedElement = div
+    if (div.classList.contains("tasks") || div.classList.contains("text")) {
+        if (div.classList.contains("tasks")) {
+            draggedElement = div
+        } else {
+            draggedElement = div.parentNode;
+        }
+        
         let box = draggedElement.getBoundingClientRect();
         offsetX = event.pageX - box.left;
         offsetY = event.pageY - box.top;
@@ -27,12 +31,18 @@ function mouseDown(event) {
 function mouseUp(event) {
     let elementUnder = document.elementsFromPoint(event.pageX, event.pageY);
 
-    if (elementUnder[0].classList.contains("tasks") && elementUnder[1].classList.contains("container")) {
-        let oldParent = draggedElement.parentNode;
-        elementUnder[1].appendChild(draggedElement);
-
-        updateContainer(oldParent);
-    } 
+    if (elementUnder[0] !== undefined) {
+        if ((elementUnder[0].classList.contains("tasks") || elementUnder[0].classList.contains("text")) && (elementUnder[1].classList.contains("container") || elementUnder[2].classList.contains("container"))) {
+            let oldParent = draggedElement.parentNode;
+            if (elementUnder[0].classList.contains("text")) {
+                elementUnder[2].appendChild(draggedElement);
+            } else {
+                elementUnder[1].appendChild(draggedElement);
+            }
+            updateContainer(oldParent);
+        } 
+    }
+    
 
     if (draggedElement) {
         draggedElement.style.position = "absolute";
@@ -44,44 +54,66 @@ function mouseUp(event) {
 }
 
 function updateContainer(obj) {
-    let topPosition = 10;
-    for (var i=0; i < obj.children.length; i++) {
+    let topPosition = 40;
+    for (var i=1; i < obj.children.length; i++) {
         let child = obj.children[i];
         child.style.position = "absolute";
         child.style.top = topPosition + "px";
         child.style.left = "10%";
 
-        topPosition += child.offsetHeight + 10;
+        topPosition += child.getBoundingClientRect().height + 10;
     }
 }
 
 function deleteTask(button) {
     let parent = button.parentNode;
     let grandparent = parent.parentNode;
-    grandparent.removeChild(parent);
-    updateContainer(grandparent);
+    let greatGrandparent = grandparent.parentNode
+    greatGrandparent.removeChild(grandparent);
+    updateContainer(greatGrandparent);
 }
 
 function addTask() {
     let container = document.querySelector(".container");
-    let newTask = document.createElement("div");
-    newTask.classList.add("tasks");
-    newTask.innerHTML = taskBase;
-    container.appendChild(newTask);
-    updateContainer(container);
+    let taskText = document.querySelector("#taskText").value;
+    let taskBase = `<div class='text'><i class='fa-solid fa-xmark' onclick='deleteTask(this)'></i>${taskText}</div>`;
+
+    if (taskText.length !== 0) {
+        let newTask = document.createElement("div");
+
+        newTask.classList.add("tasks");
+        newTask.innerHTML = taskBase;
+        container.appendChild(newTask);
+        updateContainer(container);
+    }
+    document.querySelector("#taskText").value = "";
+}
+
+function editTask(pencil) {
+    let parent = pencil.parentNode;
+    let text = parent.children[0];
+    text.contentEditable = "true";
+    text.style.border = "1px solid #a3885d";
+    text.style.backgroundColor = "#f0eadf";
+    console.log(parent.children);
 }
 
 function resizeElements() {
-    let containers = document.querySelectorAll(".container");
+    containers = document.querySelectorAll(".container");
     for (var i=0; i<containers.length; i++) {
-        for (var j=0; j<containers[i].children.length; j++) {
+        for (var j=1; j<containers[i].children.length; j++) {
             containers[i].children[j].style.width= containers[i].getBoundingClientRect().width*0.8 + "px";
             containers[i].children[j].style.left = "10%";
         }
+        updateContainer(containers[i]);
     }
 }
 
-updateContainer(document.getElementsByClassName("container")[0]);
+let containers = document.querySelectorAll(".container");
+for (var i=0; i<containers.length; i++) {
+    updateContainer(containers[i]);
+}
+
 window.addEventListener("mousedown", mouseDown);
 window.addEventListener("mouseup", mouseUp);
 window.addEventListener("resize", resizeElements);
