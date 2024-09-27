2a160a040507553d727ca4b6fab33ba82f37efb2
kyra <kjparris07@gmail.com>
2024-09-27 16:17:28 -0400

    added carousels

--- scripts.js
+++ scripts.js
@@ -1,3 +1,9 @@
+let carousels = [0, 0, 0];
+
+let carousel1 = ["imgs/carousel/carousel1/reliquary2.jpg", "imgs/carousel/carousel1/reliquary1.jpg", "imgs/carousel/carousel1/reliquary3.jpg"]
+let carousel2 = ["imgs/carousel/carousel2/foot1.jpeg", "imgs/carousel/carousel2/foot2.jpeg", "imgs/carousel/carousel2/foot3.jpeg"]
+let carousel3 = ["imgs/carousel/carousel3/bust1.jpeg", "imgs/carousel/carousel3/bust2.jpeg", "imgs/carousel/carousel3/bust3.jpeg"]
+
 function toggleNavBar() {
     var links = document.getElementById("links");
 
@@ -30,7 +36,40 @@ function handleClick() {
     closeNavBar();
 }
 
-window.addEventListener("resize", handleResize);
+function prevImage(index) {
+    let image = document.querySelectorAll(".card .carousel")[index].querySelector("img");
+    var imgArr, i;
 
-// const form = document.getElementsByTagName("form");
-// form.addEventListener("click", handleClick);
+    if (index === 0) {
+        imgArr = carousel1;
+    } else if (index === 1) {
+        imgArr = carousel2;
+    } else if (index === 2) {
+        imgArr = carousel3;
+    }
+    i = imgArr.indexOf(image.getAttribute("src"))-1;
+    if (i === -1) {
+        i = imgArr.length-1;
+    } 
+    image.src = imgArr[i];
+}
+
+function nextImage(index) {
+    let image = document.querySelectorAll(".card .carousel")[index].querySelector("img");
+    var imgArr, i;
+
+    if (index === 0) {
+        imgArr = carousel1;
+    } else if (index === 1) {
+        imgArr = carousel2;
+    } else if (index === 2) {
+        imgArr = carousel3;
+    }
+    i = imgArr.indexOf(image.getAttribute("src"))+1;
+    if (i === imgArr.length) {
+        i = 0;
+    } 
+    image.src = imgArr[i];
+}
+
+window.addEventListener("resize", handleResize);
